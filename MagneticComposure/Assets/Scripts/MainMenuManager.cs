564f17c22d21c837faa9b9786c844430c5f00702
Nicholas (Hoy) Perell <nicholas.hoy@gmail.com>
2020-06-19 16:12:49 -0400

    Updating Menu to have Credits

---
+++ MagneticComposure/Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public GameObject[] slides;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        BackToMenu();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ShowSlide(GameObject obj)
+    {
+        BackToMenu();
+        obj.SetActive(true);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void BackToMenu()
+    {
+        foreach (GameObject obj in slides)
+            obj.SetActive(false);
+    }
+}
