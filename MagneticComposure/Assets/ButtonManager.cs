36b663898dbcbe3b1f50eca535fe70e5f8e2ef1e
Nicholas (Hoy) Perell <nicholas.hoy@gmail.com>
2020-06-20 16:24:55 -0400

    Implemented Main Menu background art

---
+++ MagneticComposure/Assets/ButtonManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        collision.GetComponent<Button>().interactable = true;
+    }
+
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        collision.GetComponent<Button>().interactable = false;
+    }
+}
